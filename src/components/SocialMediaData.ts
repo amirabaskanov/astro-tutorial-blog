94426fb805224c6bcfc94fe9cec902656bc80a14
Amir Abaskanov <absknv@gmail.com>
2025-02-23 23:59:50 -0500

    Hero Section and Keyboard: Initial Commit

---
+++ src/components/SocialMediaData.ts
@@ -0,0 +1,28 @@
+import { GithubOutline } from '@icons/GithubOutline'
+import { LinkedIn } from '@icons/LinkedIn'
+import { Mail } from '@icons/Mail'
+import { Scroll } from '@icons/Scroll'
+
+export const SOCIAL_MEDIA_DATA = [
+  {
+    name: 'Github',
+    icon: GithubOutline,
+    href: 'https://github.com/amirabaskanov'
+  },
+  {
+    name: 'LinkedIn',
+    icon: LinkedIn,
+    href: 'https://linkedin.com/in/abaskanov'
+  },
+  {
+    //Change to Quick View and Download
+    name: 'Resume',
+    icon: Scroll,
+    href: 'https://read.cv/'
+  },
+  {
+    name: 'Email',
+    icon: Mail,
+    href: 'mailto:me@amirabaskanov@gmail.com'
+  }
+]
